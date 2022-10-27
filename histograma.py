92a0e809a6095c49e2032008504108b44c9ea10a
Michael <91438312+michaeljmcardoso@users.noreply.github.com>
2022-10-27 14:29:20 -0300

    Update histograma.py

--- histograma.py
+++ histograma.py
@@ -1,9 +1,12 @@
+# importar os pacotes necessários
 from matplotlib import pyplot as plt
 
-# valores do eixo-y
+# definindo os valores do eixo-y
 y = [13, 1, 2, 6, 4]
+
 # Função para plotar o histograma
 plt.hist(y)
+
 # Função para mostrar o plot
 plt.show()
 
